80b09d6eba60cca98026a814871c88d6cc00424d
Marlon Gayle <marlon.gayle@live.com>
2021-02-23 17:52:28 -0500

    Update settings.py
    updated ALLOWED_HOSTS to '*'

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
